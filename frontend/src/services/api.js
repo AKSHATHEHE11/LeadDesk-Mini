f185fe5e99c874b1296cae06798f1f960e6b5763
Akshat Agrawal <work.brayill@gmail.com>
2026-07-25 18:32:02 +0530

    Update frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Sends the JWT token with every request
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://leaddesk-mini.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
